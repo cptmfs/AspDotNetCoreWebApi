cc61a0954f357af706e58f7f9f13159e9a4a646f
Muhammed Ferit Simsek <cpt.mfs@gmail.com>
2023-06-12 13:41:50 +0300

    v_bsStoreApp

---
+++ Applications/bsStoreApp/WebApi/Repositories/RepositoryContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApi.Models;
+
+namespace WebApi.Repositories
+{
+    public class RepositoryContext:DbContext
+    {
+        public RepositoryContext(DbContextOptions<RepositoryContext> options):base (options)
+        {
+                
+        }
+        public DbSet<Book> Books { get; set; }
+    }
+}
